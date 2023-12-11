b9288e10d06877f138403959ba4add9c3cf2e247
JakauBahuslau1 <rct.leonenko@bsu.by>
2023-12-11 20:23:08 +0300

    Исправлена лаба 5

--- chislaki_5/funcs.cpp
+++ chislaki_5/funcs.cpp
@@ -27,3 +27,40 @@ double simpson(const double& a, const double& b, const int& n) {
     }
     return h * sum / 3;
 }
+
+double double_simpson_method(const double& a, const double& b, const double& c, const double& d, const int& n) {
+	double hx = (b - a) / (2 * n);
+	double hy = (d - c) / (2 * n);
+
+	double result = 0.0;
+
+	for (int i = 0; i <= n - 1; ++i) {
+		for (int j = 0; j <= n - 1; ++j) {
+			result += f(a + (double(i) * 2) * hx, c + (double(j) * 2) * hy);
+			result += 4 * f(a + (double(i) * 2 + 1) * hx, c + (double(j) * 2) * hy);
+			result += f(a + (double(i) * 2 + 2) * hx, c + (double(j) * 2) * hy);
+			result += 4 * f(a + (double(i) * 2) * hx, c + (double(j) * 2 + 1) * hy);
+			result += 16 * f(a + (double(i) * 2 + 1) * hx, c + (double(j) * 2 + 1) * hy);
+			result += 4 * f(a + (double(i) * 2 + 2) * hx, c + (double(j) * 2 + 1) * hy);
+			result += f(a + (double(i) * 2) * hx, c + (double(j) * 2 + 2) * hy);
+			result += 4 * f(a + (double(i) * 2 + 1) * hx, c + (double(j) * 2 + 2) * hy);
+			result += f(a + (double(i) * 2 + 2) * hx, c + (double(j) * 2 + 2) * hy);
+		}
+	}
+	return hx * hy * result / 9;
+}
+
+double calculate_double_simpson(const double& a, const double& b, const double& c, const double& d, const double& epsilon) {
+	long n = 10;
+	double inter1 = 0;
+	double inter2 = double_simpson_method(a, b, c, d, n);
+
+	while (abs(inter1 - inter2) > 15 * epsilon) {
+		n *= 2;
+		inter1 = inter2;
+		inter2 = double_simpson_method(a, b, c, d, n);
+	}
+
+	return inter2;
+}
+
--- chislaki_5/funcs.h
+++ chislaki_5/funcs.h
@@ -4,5 +4,6 @@
 using namespace std;
 
 double f(const double& x);
+double f(const double& x, const double& y);
 double trapezoidal(const double& a, const double& b, const int& n);
 double simpson(const double& a, const double& b, const int& n);
